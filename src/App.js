2d5c11860e9ec33ac8f62a59127b9eca8e278d9c
lalitha1900 <trlalitha2000@gmail.com>
2024-03-26 01:26:40 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
+export default function App() {
+  function showFullName() {
+    const firstName = document.getElementsByName("firstName")[0].value;
+    const lastName = document.getElementsByName("lastName")[0].value;
+    //alert(firstName + " " + lastName);
+    if (firstName === "" || lastName === "") {
+      document.getElementById("fullName").style.display = "none";
+      return;
+    }
+    document.getElementById("fullName").style.display = "block";
+    document.getElementById("fullName").innerHTML =
+      "Full Name: " + firstName + " " + lastName;
+  }
+  function submit(event) {
+    event.preventDefault();
+    return false;
+  }
+  useEffect(() => {
+    document.getElementById("formId").addEventListener("submit", submit);
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Full Name Display</h1>
+      <form id="formId">
+        <label for="firstName">First Name :</label>
+        <input type="text" name="firstName" required />
+        <br />
+        <label for="lastName">Last Name :</label>
+        <input type="text" name="lastName" required />
+        <br />
+        <button type="submit" onClick={showFullName}>
+          Submit
+        </button>
+        <br />
+        <br />
+        <div id="fullName" style={{ display: "none" }}>
+          Full Name:
+        </div>
+      </form>
     </div>
   );
 }
 
-export default App;
